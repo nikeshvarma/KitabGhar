eeef70d26e33c54c871e085bf75ca80c3e6c985b
Nikesh Varma <nikeshvarma50@gmail.com>
2022-01-28 12:22:42 +0530

    Preloader

--- HOME/urls.py
+++ HOME/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home_view')
+    path('', views.HomeView.as_view(), name='home_view'),
+    path('about/', views.AboutView.as_view(), name='about_view'),
+    path('contact-us/', views.ContactView.as_view(), name='contact_us_view')
 ]
--- HOME/views.py
+++ HOME/views.py
@@ -9,3 +9,11 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['slider_images'] = SliderImages.objects.all()
         return context
+
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'home/contact_us.html'
